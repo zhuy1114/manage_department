5267af048ba0f2f6263b9ffc4804f4cd66dfbbfe
zhuy <zhuy_ing@163.com>
2020-06-12 23:02:14 +0800

    baogao

--- run_suite.py
+++ run_suite.py
@@ -10,7 +10,7 @@ from app import BASE_DIR
 suite = unittest.TestLoader().discover(BASE_DIR + "/script", pattern="test*.py")
 
 # 设置报告名称
-file_name = "/report-{}.html".format(time.strftime("%Y%m%d%H%M%S"))
+file_name = "/report.html"
 
 # 生成报告
 BeautifulReport(suite).report(filename=file_name, description="ihrm部门管理", log_path=BASE_DIR + "/report")
